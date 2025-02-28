d4a8d9e52c8970c984cfd35886308c878452832c
quispchc <quisp@carlingtonchc.org>
2025-02-28 10:25:25 -0500

    production deployment chnages

--- src/config/database.js
+++ src/config/database.js
@@ -1,13 +1,23 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
+let host, port;
+
+if (process.env.NODE_ENV === 'development') {
+  host = 'localhost';
+  port = process.env.DB_PORT || 5432;
+} else {
+  host = `/cloudsql/${process.env.DB_INSTANCE}`;
+  port = null;
+}
+
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
   process.env.DB_PASSWORD,
   {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+    host: host,
+    port: port,
     dialect: 'postgres',
     logging: false,
   }
