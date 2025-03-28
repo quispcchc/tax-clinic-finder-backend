5bbffd5fa649e8e440ecdd5a112f0b69f2b470cf
quispchc <quisp@carlingtonchc.org>
2025-03-28 18:59:28 -0400

    url changes

--- src/app.js
+++ src/app.js
@@ -1,14 +1,26 @@
 const express = require('express');
 const cors = require('cors');
-const routes = require('./routes');
+const routes = require('./routes/authRoutes');
 const { errorHandler } = require('./middlewares/errorMiddleware');
 
 const app = express();
 
-app.use(cors());
+const corsOptions = {
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+  };
+  
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
+
 app.use(express.json());
 
 app.use('/api', routes);
+
+app.get('/api/health', (req, res) => {
+    res.send('Backend is running');
+});
 
 app.use(errorHandler);
 
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,5 +1,21 @@
 const express = require('express');
+const { logger } = require("../config/logger");
+const authMiddleware = require("../middlewares/auth");
 const { register, login, resetPassword,  resetPasswordWithToken, changePassword } = require('../controllers/authController');
+const {
+    getTaxClinics,
+    updateAppointmentAvailability,
+    getUsers,
+    createTaxClinic,
+    updateTaxClinic,
+    deleteTaxClinic,
+    createUser,
+    updateUser,
+    deleteUser,
+    saveFilteredData,
+    exportClientLogs,
+    updateFilteredData,
+  } = require("../controllers/clinicController");
 
 const router = express.Router();
 
@@ -8,5 +24,17 @@ router.post('/login', login);
 router.post('/reset-password', resetPassword);
 router.post('/reset-password/:token', resetPasswordWithToken);
 router.put('/change-password/:userId', changePassword);
+router.get('/tax-clinics', getTaxClinics);
+router.post('/create-clinic', createTaxClinic);
+router.put('/:id', updateTaxClinic);
+router.delete('/:id', deleteTaxClinic);
+router.put('/update-availability/:id', updateAppointmentAvailability);
+router.get('/users', getUsers);
+router.post('/create-user', createUser);
+router.put('/users/:id', updateUser);
+router.delete('/users/:id', deleteUser);
+router.post('/save-filters', saveFilteredData);
+router.put('/update-filters/:id', updateFilteredData);
+router.get('/export-logs', exportClientLogs);
 
 module.exports = router;
